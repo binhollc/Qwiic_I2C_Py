1f82e4c849bdf2330310963d1495180fb6e58bfe
Jonathan Georgino <jonathan@binho.io>
2019-09-28 14:04:03 -0700

    Changed the version number to begin with 255 to indicate it's a binho development build

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.8.4',
+    version='255.8.4',
 
     description='SparkFun Electronics qwiic I2C library',
     long_description=long_description,
